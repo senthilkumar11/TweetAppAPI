da992dc8b0c726f6f9185342f8344c697e1d379e
AAP49ZZ <sselvaraj4.cw@mmm.com>
2021-05-12 20:11:01 +0530

    added data for role

--- src/main/java/com/app/tweet/TweetApplication.java
+++ src/main/java/com/app/tweet/TweetApplication.java
@@ -1,13 +1,40 @@
 package com.app.tweet;
 
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.app.tweet.model.*;
+import com.app.tweet.repo.RoleRepository;
 
 @SpringBootApplication
 public class TweetApplication {
-
+	
+	@Autowired
+	RoleRepository roleRepository;
 	public static void main(String[] args) {
 		SpringApplication.run(TweetApplication.class, args);
 	}
 
+	@Bean
+	InitializingBean sendDatabase() {
+		return ()->{
+			Optional<Role> admin= roleRepository.findByName(ERole.ROLE_ADMIN);
+			if(!admin.isPresent()) {
+				roleRepository.save(new Role(ERole.ROLE_ADMIN));
+			}
+			Optional<Role> user= roleRepository.findByName(ERole.ROLE_USER);
+			if(!user.isPresent()) {
+				roleRepository.save(new Role(ERole.ROLE_USER));
+			}
+			System.out.println("Role save completed");
+		};
+	}
+	
 }
